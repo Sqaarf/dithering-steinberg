5034b25ebf896f10015ed2a2d8e8026a3a10ecc2
Sqaarf <mrscarf19@gmail.com>
2021-11-11 11:32:50 +0100

    Method refactoring & tqdm progress bar

--- main.py
+++ main.py
@@ -1,13 +1,24 @@
 import numpy as np
 from PIL import Image
+from tqdm import tqdm
 
-img = Image.open('./images/Guts.png')
+img = Image.open('./images/landscape.jpg')
 array = np.array(img)
 shape = array.shape
 
-factor = 4
+factor = int(input("Factor :"))
 
-for y in range(array.shape[1]-1):
+def quant_err(color, coeff, err):
+	c = color
+	r = color[0]
+	g = color[1]
+	b = color[2]
+	r = r + err[0] * coeff/16
+	g = g + err[1] * coeff/16
+	b = b + err[2] * coeff/16
+	return [r,g,b]
+
+for y in tqdm(range(array.shape[1]-1)):
 	for x in range(1, array.shape[0]-1):
 		oldR = array[x][y][0]
 		oldG = array[x][y][1]
@@ -22,43 +33,13 @@ for y in range(array.shape[1]-1):
 		errR = oldR - newR
 		errB = oldG - newG
 		errG = oldG - newG
+		err = (errR, errB, errG)
 
-		c = array[x+1][y]
-		r = array[x+1][y][0]
-		g = array[x+1][y][1]
-		b = array[x+1][y][2]
-		r = r + errR * 7/16
-		g = g + errG * 7/16
-		b = b + errB * 7/16
-		array[x+1][y] = [r,g,b]
-
-		c = array[x-1][y+1]
-		r = array[x-1][y+1][0]
-		g = array[x-1][y+1][1]
-		b = array[x-1][y+1][2]
-		r = r + errR * 3/16
-		g = g + errG * 3/16
-		b = b + errB * 3/16
-		array[x-1][y+1] = [r,g,b]
-
-		c = array[x][y+1]
-		r = array[x][y+1][0]
-		g = array[x][y+1][1]
-		b = array[x][y+1][2]
-		r = r + errR * 5/16
-		g = g + errG * 5/16
-		b = b + errB * 5/16
-		array[x][y+1] = [r,g,b]
-
-		c = array[x+1][y+1]
-		r = array[x+1][y+1][0]
-		g = array[x+1][y+1][1]
-		b = array[x+1][y+1][2]
-		r = r + errR * 7/16
-		g = g + errG * 7/16
-		b = b + errB * 7/16
-		array[x+1][y+1] = [r,g,b]
+		array[x+1][y] = quant_err(array[x+1][y], 7, err)
+		array[x-1][y+1] = quant_err(array[x-1][y+1], 3, err)
+		array[x][y+1] = quant_err(array[x][y+1], 5, err)
+		array[x+1][y+1] = quant_err(array[x+1][y+1], 7, err)
 
 invimg = Image.fromarray(array)
-invimg.save('./images/test.png')
+invimg.save('./images/dithered.png')
 print("done.")
